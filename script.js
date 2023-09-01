47d687ba2c8c74b914c8881d2f066510fc952972
Lokesh Sharma <90918725+slokesh2905@users.noreply.github.com>
2023-09-01 18:42:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+
+function fetchData() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => {
+            // Handle the data here
+            displayData(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+
+function displayData(posts) {
+    const postList = document.getElementById('post-list');
+
+    // Loop through the posts and create list items
+    posts.forEach(post => {
+        const listItem = document.createElement('li');
+        listItem.textContent = post.title;
+        postList.appendChild(listItem);
+    });
+}
+
+
+fetchData();
